c611295f792f0389113255727f5a47867e91c7e0
sahilsoni754 <106666226+sahilsoni754@users.noreply.github.com>
2023-09-03 19:18:51 +0530

    checkout.js

--- pages/checkout.js
+++ pages/checkout.js
@@ -13,13 +13,13 @@ const Checkout = ({cart, subTotal, addToCart, removeFromCart}) => {
             <div className="mx-auto flex my-2">
                 <div className="px-2 w-1/2">
                     <div class="mb-4">
-                        <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
+                        <label for="className" class="leading-7 text-sm text-gray-600">Name</label>
                         <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                     </div>
 
                 </div>
                 <div className="px-2 w-1/2">
-                    <div class="mb-4">
+                    <div className="mb-4">
                         <label for="Email" class="leading-7 text-sm text-gray-600">Email</label>
                         <input type="text" id="Email" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                     </div>
@@ -27,40 +27,40 @@ const Checkout = ({cart, subTotal, addToCart, removeFromCart}) => {
                 </div>
                 </div>
                 <div className="px-2 w-full ">
-                    <div class="mb-4">
-                        <label for="address" class="leading-7 text-sm text-gray-600">Address</label>
+                    <div className="mb-4">
+                        <label for="address" className="leading-7 text-sm text-gray-600">Address</label>
                         <textarea name="address"  id="address" rows="2" cols="30"  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></textarea>
                     </div>
 
                 </div>
                 <div className="mx-auto flex my-2">
                 <div className="px-2 w-1/2">
-                    <div class="mb-4">
-                        <label for="phone" class="leading-7 text-sm text-gray-600">phone</label>
-                        <input type="phone" id="phone" name="phone" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    <div className="mb-4">
+                        <label for="phone" className="leading-7 text-sm text-gray-600">phone</label>
+                        <input type="phone" id="phone" name="phone" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                     </div>
 
                 </div>
                 <div className="px-2 w-1/2">
-                    <div class="mb-4">
-                        <label for="City" class="leading-7 text-sm text-gray-600">City</label>
-                        <input type="text" id="City" name="City" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    <div className="mb-4">
+                        <label for="City" className="leading-7 text-sm text-gray-600">City</label>
+                        <input type="text" id="City" name="City" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                     </div>
 
                 </div>
                 </div>
                 <div className="mx-auto flex my-2">
                 <div className="px-2 w-1/2">
-                    <div class="mb-4">
-                        <label for="State" class="leading-7 text-sm text-gray-600">State</label>
-                        <input type="text" id="State" name="State" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    <div className="mb-4">
+                        <label for="State" className="leading-7 text-sm text-gray-600">State</label>
+                        <input type="text" id="State" name="State" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                     </div>
 
                 </div>
                 <div className="px-2 w-1/2">
-                    <div class="mb-4">
-                        <label for="pincode" class="leading-7 text-sm text-gray-600">pincode</label>
-                        <input type="text" id="pincode" name="pincode" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    <div className="mb-4">
+                        <label for="pincode" className="leading-7 text-sm text-gray-600">pincode</label>
+                        <input type="text" id="pincode" name="pincode" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                     </div>
 
                 </div>
